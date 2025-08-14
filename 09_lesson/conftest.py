9a2182e24b4c24f9c5d5a04f6d1b56e3ab1c1943
janealex1810 <janealex1810@gmaile.com>
2025-08-14 16:39:56 +0400

    Внесены изменения

--- 09_lesson/conftest.py
+++ 09_lesson/conftest.py
@@ -1,18 +1,8 @@
 import pytest
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models_student import Base, Student
 
 
 DB_URL = "postgresql://myuser:mypassword@localhost:5432/mydatabase"
-
-
-@pytest.fixture(scope="module")
-def engine():
-    engine = create_engine(DB_URL)
-    Base.metadata.create_all(engine)  # Создаем таблицы
-    yield engine
-    Base.metadata.drop_all(engine)  # Удаляем таблицы после всех тестов
 
 
 @pytest.fixture
